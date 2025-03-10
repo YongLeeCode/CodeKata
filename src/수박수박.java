bfeb96b33e618d002bc55388092d0021d4451065
YongLeeCode <yonghwan311@gmail.com>
2025-03-10 09:20:38 +0900

    feat: 수박수박 문제 해결

--- src/수박수박.java
+++ src/수박수박.java
@@ -1,4 +1,17 @@
-package PACKAGE_NAME;
-
 public class 수박수박 {
+    public static String solution(int n) {
+        StringBuilder sb = new StringBuilder();
+        for(int i = 0; i < n; i++) {
+            if(i % 2 == 0) {
+                sb.append("수");
+            } else {
+                sb.append("박");
+            }
+        }
+        return sb.toString();
+    }
+    public static void main(String[] args) {
+         String result = solution(5);
+         System.out.println(result);
+    }
 }
