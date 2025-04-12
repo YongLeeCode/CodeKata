b0c1e3520698160e58bfcc520c35b783bd1b968f
YongLeeCode <yonghwan311@gmail.com>
2025-04-12 23:04:04 +0900

    feat: 성격_유형_검사하기 문제 해결

---
+++ src/성격_유형_검사하기.java
@@ -0,0 +1,10 @@
+package PACKAGE_NAME;
+
+/**
+ * @packageName    : PACKAGE_NAME
+ * @fileName       : 성격_유형_검사하기
+ * @author         : yong
+ * @date           : 4/12/25
+ * @description    :
+ */public class 성격_유형_검사하기 {
+}
