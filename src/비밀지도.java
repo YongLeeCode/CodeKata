f773ee0a81909e24f95f8eda53f6d087f5e25d38
YongLeeCode <yonghwan311@gmail.com>
2025-05-09 23:49:32 +0900

    feat: 비밀지도 문제 해결

--- src/비밀지도.java
+++ src/비밀지도.java
@@ -1,2 +1,19 @@
+import java.util.Arrays;
+
 public class 비밀지도 {
+	public static String[] solution(int n, int[] arr1, int[] arr2){
+		String[] arr3 = new String[n];
+		for(int i = 0; i < n; i++) {
+			arr3[i] = String.format("%" + n + "s", Integer.toBinaryString(arr1[i] | arr2[i])).replace(" ", "0");
+			arr3[i] = arr3[i].replace("1", "#").replace("0", " ");
+		}
+		return arr3;
+	}
+
+	public static void main(String[] args) {
+		int n = 5;
+		int[] arr1 = {9, 20, 28, 18, 11};
+		int[] arr2 = {30, 1, 21, 17, 28};
+		System.out.println(Arrays.toString(solution(n, arr1, arr2)));
+	}
 }
