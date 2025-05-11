4f37c40eb1e0cf4e21104e45a9edcec756eb2f07
YongLeeCode <yonghwan311@gmail.com>
2025-05-11 20:53:10 +0900

    feat: 부족한 금액 계산하기 문제 생성

---
+++ src/부족한금액계산하기.java
@@ -0,0 +1,11 @@
+public class 부족한금액계산하기 {
+	public static long solution(int price, int money, int count) {
+		long answer = -1;
+
+		return answer;
+	}
+
+	public static void main(String[] args) {
+		System.out.println(solution(3, 20, 4));
+	}
+}
