af4ec70b60fccf127895614235d248300bfe86de
YongLeeCode <yonghwan311@gmail.com>
2025-05-13 22:13:07 +0900

    feat: 시간초과 문제 해결을 한 기사단원의 무기 문제 해결

--- src/기사단원의무기.java
+++ src/기사단원의무기.java
@@ -2,9 +2,10 @@ import java.util.Arrays;
 
 public class 기사단원의무기 {
 	public static void main(String[] args) {
-		System.out.println(solution(10, 3, 2));
+		System.out.println(solution2(10, 3, 2));
 	}
 
+	// O(N^2)이라 시간초과 문제 발생
 	public static int solution(int number, int limit, int power) {
 
 		int[] arr = new int[number];
@@ -20,5 +21,27 @@ public class 기사단원의무기 {
 		}
 
 		return Arrays.stream(arr).sum();
+	}
+
+	public static int solution2(int number, int limit, int power) {
+		int[] divisorCount = new int[number + 1];
+
+		// 배수를 활용해 약수 개수 계산
+		for (int i = 1; i <= number; i++) {
+			for (int j = i; j <= number; j += i) {
+				divisorCount[j]++;
+			}
+		}
+
+		int total = 0;
+		for (int i = 1; i <= number; i++) {
+			if (divisorCount[i] > limit) {
+				total += power;
+			} else {
+				total += divisorCount[i];
+			}
+		}
+
+		return total;
 	}
 }
