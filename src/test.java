d3b59d629467b0b49c44e5a5cb915cfb02b29d6e
YongLeeCode <yonghwan311@gmail.com>
2025-06-06 23:16:28 +0900

    feat: 균형잡힌 세상 문제 구현

--- src/test.java
+++ src/test.java
@@ -1,4 +1,35 @@
-package PACKAGE_NAME;
-
 public class test {
+	//옹알이
+	public static int solution(String[] babbling) {
+		String[] sounds = {"aya", "ye", "woo", "ma"};
+		int count = 0;
+
+		for(String b : babbling) {
+			boolean isValid = true;
+			String lastword = "";
+			while(!b.isEmpty()) {
+				boolean matched = false;
+				for(String sound : sounds) {
+					if(b.startsWith(sound) && !sound.equals(lastword)) {
+						b = b.substring(sound.length());
+						lastword = sound;
+						matched = true;
+						break;
+					}
+				}
+				if(!matched) {
+					isValid = false;
+					break;
+				}
+			}
+			if(isValid) {
+				count++;
+			}
+		}
+		return count;
+	}
+	public static void main(String[] args) {
+		String[] babblings = {"ayaye", "uuu", "yeye", "yemawoo", "ayaayaa"};
+		System.out.println(solution(babblings));
+	}
 }
