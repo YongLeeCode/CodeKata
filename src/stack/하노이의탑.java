8fdafcb83d9468fcc24f6d2f9ac30660561b03b8
YongLeeCode <yonghwan311@gmail.com>
2025-06-22 21:35:38 +0900

    feat: 하노이의 탑 문제 생성

---
+++ src/stack/하노이의탑.java
@@ -0,0 +1,15 @@
+package stack;
+
+import java.util.Arrays;
+
+public class 하노이의탑 {
+	private static int[][] solution(int n) {
+		int[][] answer = {};
+		return answer;
+	}
+
+	public static void main(String[] args) {
+		System.out.println(Arrays.deepToString(solution(2)));
+
+	}
+}
