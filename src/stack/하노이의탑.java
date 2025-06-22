449471e2f7e41500734b536ff3064870381008e5
YongLeeCode <yonghwan311@gmail.com>
2025-06-22 22:01:02 +0900

    feat: 하노이의 탑 문제 해결

--- src/stack/하노이의탑.java
+++ src/stack/하노이의탑.java
@@ -1,15 +1,38 @@
 package stack;
 
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 
 public class 하노이의탑 {
+
+	static List<int[]> sequence;
+
 	private static int[][] solution(int n) {
-		int[][] answer = {};
+ 		sequence = new ArrayList<>();
+		hanoi(n, 1, 2, 3);
+
+		int[][] answer = new int[sequence.size()][2];
+		for(int i = 0; i < sequence.size(); ++i) {
+			int[] cmd = sequence.get(i);
+			answer[i][0] = cmd[0];
+			answer[i][1] = cmd[1];
+		}
+
 		return answer;
 	}
 
-	public static void main(String[] args) {
-		System.out.println(Arrays.deepToString(solution(2)));
+	private static void hanoi(int n, int from, int via, int to) {
+		if (n == 0) return;
 
+		int[] move = {from, to};
+		hanoi(n - 1, from, to, via);
+		sequence.add(move);
+		hanoi(n - 1, to, via, from);
+	}
+
+	public static void main(String[] args) {
+		int[][] answer = solution(2);
+		System.out.println(Arrays.deepToString(answer));
 	}
 }
