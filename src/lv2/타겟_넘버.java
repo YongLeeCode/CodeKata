e402f0c6f7b9a1e685040e97ca1ef2eee6f6d153
YongLeeCode <yonghwan311@gmail.com>
2025-08-07 19:56:03 +0900

    타겟넘버 문제

--- src/lv2/타겟_넘버.java
+++ src/lv2/타겟_넘버.java
@@ -1,4 +1,20 @@
 package lv2;
 
 public class 타겟_넘버 {
+	private static int solution(int[] numbers, int target) {
+		return dfs(0, 0, numbers, target);
+	}
+	private static int dfs(int depth, int sum, int[] numbers, int target) {
+		//종료 시점
+		if(depth == numbers.length) {
+			return sum == target ? 1 : 0;
+		}
+
+		int plus = dfs(depth + 1, sum + numbers[depth], numbers, target);
+		int minus = dfs(depth + 1, sum - numbers[depth], numbers, target);
+		return plus + minus;
+	}
+	public static void main(String[] args) {
+		solution(new int[] {1,1,1,1,1}, 3);
+	}
 }
