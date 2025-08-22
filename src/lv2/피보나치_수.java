33ce668c413370e6631cab32412b1cf1fe56e790
YongLeeCode <yonghwan311@gmail.com>
2025-08-22 16:19:19 +0900

    피보나치 수 문제

--- src/lv2/피보나치_수.java
+++ src/lv2/피보나치_수.java
@@ -1,4 +1,20 @@
 package lv2;
 
 public class 피보나치_수 {
+	private static int solution(int n) {
+		if(n < 2) return n;
+
+		long a = 0;
+		long b = 1;
+
+		for(int i = 2; i <= n; i++) {
+			long temp = b;
+			b = (a + b) % 1234567;
+			a = temp;
+		}
+		return (int) b;
+	}
+	public static void main(String[] args) {
+		System.out.println(solution(99));
+	}
 }
